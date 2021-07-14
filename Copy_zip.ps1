6758c08cd12b90edd862d9db711daa99064aff05
thomas ong <22740191+thomasong0204@users.noreply.github.com>
2021-07-14 15:22:11 +0800

    Create Copy_zip.ps1
    Sample script to copy file of certain extension  and zip it

---
+++ Copy_zip.ps1
@@ -0,0 +1,22 @@
+######################################################
+#  Sample powerscript to extract and upload data
+######################################################
+$Last30Days = (Get-Date).AddDays(-30)
+$tempFinalFolder = C:\Temp\log
+$a = "{0:d}" -f (get-date)
+$rootFolder = "C:\Users\xxx\Downloads"
+#$ZipFile = G:\Project\extract_$(Get-Date -format 'u').zip
+$FileList = Get-ChildItem -Path $rootFolder\* -Include *.pdf | Where-Object {$_.LastWriteTime -gt $Last30Days}
+
+#Start to copy file into temo
+foreach($fileEa in $FileList)
+{
+    Copy-Item "$fileEa" -destination $tempFinalFolder
+}
+
+# Start to zip
+Compress-Archive -Path $tempFinalFolder -DestinationPath G:\Project\extract_WB1_$a.zip -Force
+
+
+
+
